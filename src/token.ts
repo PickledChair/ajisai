decb627e273fc649a2ca81b960e98838d7959baf
PickledChair <ubatamamoon@gmail.com>
2024-06-15 20:13:12 +0900

    val をキーワードに追加

--- src/token.ts
+++ src/token.ts
@@ -3,7 +3,7 @@ export type TokenType =
   | "," | ":" | ";" | "|" | "(" | ")" | "{" | "}"
   | "->"
   | "true" | "false"
-  | "else" | "if" | "let" | "func"
+  | "else" | "if" | "let" | "func" | "val"
   | "identifier" | "integer" | "string"
   | "eof";
 
@@ -13,6 +13,6 @@ export type Token = Readonly<MutableToken>;
 export const printToken = (token: Token) => console.log(`Token { tokenType: ${token.tokenType}, value: ${token.value} }`);
 
 export type Keyword = Extract<TokenType, (typeof keywords)[number]>;
-const keywords = ["true", "false", "else", "if", "let", "func"] as const;
+const keywords = ["true", "false", "else", "if", "let", "func", "val"] as const;
 
 export const isKeyword = (s: string): Keyword | null => (keywords as Readonly<string[]>).includes(s) ? s as Keyword : null;
